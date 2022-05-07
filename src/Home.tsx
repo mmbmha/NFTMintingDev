44e055d1b53df0f352b8e695c33b21345d66fda1
Mohanad Hassan <mmbmha@Mohanads-MacBook-Pro.local>
2022-05-07 14:14:23 +0300

    add logo

--- src/Home.tsx
+++ src/Home.tsx
@@ -592,7 +592,7 @@ const Home = (props: HomeProps) => {
                             <br/>
                             <div><Price
                                 label={isActive && whitelistEnabled && (whitelistTokenBalance > 0) ? (whitelistPrice + " " + priceLabel) : (price + " " + priceLabel)}/><Image
-                                src="cool-cats.gif"
+                                src="all.gif"
                                 alt="NFT To Mint"/></div>
                             <br/>
                             {wallet && isActive && whitelistEnabled && (whitelistTokenBalance > 0) && isBurnToken &&
